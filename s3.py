d4433ab92b8ca365241433770d32b866350d26b9
Lucas-Barros <lbarros>
2024-04-06 22:41:47 -0300

    s3-list

---
+++ s3.py
@@ -0,0 +1,19 @@
+import boto3
+
+def listar_buckets_s3(profile_name='cockpit'):
+    session = boto3.Session(profile_name=profile_name)
+    s3_client = session.client('s3')
+    response = s3_client.list_buckets()
+    buckets_details = []
+    for bucket in response['Buckets']:
+        bucket_details = {
+            'Nome do bucket': bucket['Name'],
+            'Data de criação': bucket['CreationDate']
+        }
+        buckets_details.append(bucket_details)
+    return buckets_details
+
+# Exemplo de uso
+buckets = listar_buckets_s3()
+for bucket in buckets:
+    print(bucket)
